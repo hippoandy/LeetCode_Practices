66c0796f1745607c92284d4a6636fcf42ca67c8c
hippoandy <ycaho@ucdavis.edu>
2019-07-08 16:53:30 -0700

    New practices

---
+++ leetcode/src/app/136_Single_Number.java
@@ -0,0 +1,26 @@
+package app;
+
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int singleNumber(int[] nums)
+    {
+        HashMap<Integer, Integer> dict = new HashMap<Integer, Integer>();
+
+        for( int i = 0; i < nums.length; i++ )
+        {
+            if( dict.containsKey( nums[ i ] ) )
+                dict.put( nums[ i ], dict.get( nums[ i ] ) + 1 );
+            else
+                dict.put( nums[ i ], 1 );
+        }
+
+        for( Map.Entry<Integer, Integer> entry : dict.entrySet() )
+        {
+            if( entry.getValue() < 2 )
+                return entry.getKey();
+        }
+        return Integer.MIN_VALUE;
+    }
+}
---
+++ leetcode/src/app/226_Invert_Binary_Tree.java
@@ -0,0 +1,32 @@
+package app;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode invertTree(TreeNode root)
+    {
+        if( root == null ) return null;
+
+        TreeNode right = invertTree( root.right );
+        TreeNode left = invertTree( root.left );
+        root.right = left;
+        root.left = right;
+        
+        return root;
+    }
+}
