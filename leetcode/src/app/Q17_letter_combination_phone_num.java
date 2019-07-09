1cf1d088f3126bbb6620be82ef48d0ac2511d57f
hippoandy <ycaho@ucdavis.edu>
2019-07-09 16:50:46 -0700

    Revisited 20190709

--- leetcode/src/app/Q17_letter_combination_phone_num.java
+++ leetcode/src/app/Q17_letter_combination_phone_num.java
@@ -1,34 +1,14 @@
 package app;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
-class Q17_letter_combination_phone_num
-{
-    HashMap<String, String> map = new HashMap<String, String>();
-    List<String> output = new ArrayList<String>();
-
-    public void backtrack( String comb, String remaining )
-    {
-        // if there is no more digits to check
-        if ( remaining.length() == 0 ) output.add( comb ); // the comb is done
-        else
-        {
-            // iterate over all letters which map the next available digit
-            String digit = remaining.substring( 0, 1 );
-            String letters = map.get( digit );
-            for( int i = 0; i < letters.length(); i++ )
-            {
-                String l = map.get( digit ).substring( i, i + 1 );
-                // append the current letter to the comb and proceed to the next digits
-                backtrack( comb + l, remaining.substring( 1 ) );
-            }
-        }
-    }
+class Q17_letter_combination_phone_num {
+    Map<String, String> map = new HashMap<String, String>();
+    List<String> sol = new ArrayList<String>();
 
     public List<String> letterCombinations(String digits)
     {
+        // build the map
         map.put( "2", "abc" );
         map.put( "3", "def" );
         map.put( "4", "ghi" );
@@ -37,8 +17,28 @@ class Q17_letter_combination_phone_num
         map.put( "7", "pqrs" );
         map.put( "8", "tuv" );
         map.put( "9", "wxyz" );
-
+        
+        // start
         if( digits.length() != 0 ) backtrack( "", digits );
-        return output;
+        return sol;
+    }
+    
+    public void backtrack( String comb, String remains )
+    {
+        if( remains.length() == 0 )
+        {
+            sol.add( comb );
+            return;
+        }
+        
+        String digit = remains.substring( 0, 1 );
+        String letters = map.get( digit );
+
+        for( int i = 0; i < letters.length(); i++ )
+        {
+            String l = letters.substring( i, i+1 );
+            // add the letter to comb then recursive call
+            backtrack( comb + l, remains.substring( 1 ) );
+        }
     }
 }
