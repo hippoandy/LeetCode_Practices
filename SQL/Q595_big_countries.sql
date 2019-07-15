895b5a88583a7d8a165ef40de301f2dbf412d280
hippoandy <ycaho@ucdavis.edu>
2019-07-14 21:55:09 -0700

    Add reference

--- SQL/Q595_big_countries.sql
+++ SQL/Q595_big_countries.sql
@@ -1,3 +1,5 @@
+# ref. https://leetcode.com/problems/big-countries/discuss/103561/Union-and-OR-and-the-Explanation
+
 # OR
 # SELECT name, population, area FROM World WHERE population > 25000000 OR area > 3000000;
 
