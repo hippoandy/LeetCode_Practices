b93faa245f34e47a1b967afa32d3d8e75a294c22
hippoandy <ycaho@ucdavis.edu>
2019-07-14 21:53:39 -0700

    New practices 20190714

---
+++ SQL/Q595_big_countries.sql
@@ -0,0 +1,13 @@
+# OR
+# SELECT name, population, area FROM World WHERE population > 25000000 OR area > 3000000;
+
+# UNION
+SELECT name, population, area
+FROM World
+WHERE area > 3000000 
+
+UNION
+
+SELECT name, population, area
+FROM World
+WHERE population > 25000000;
