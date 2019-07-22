1f12fc7a6f936cfc77fac3fe4e63fb75f83dc106
hippoandy <hippo1993928@gmail.com>
2019-07-22 12:43:08 -0700

    New practices 20190722

---
+++ Java/src/app/Q509_fibonacci_num.java
@@ -0,0 +1,19 @@
+package app;
+
+class Q509_fibonacci_num
+{
+    public int fib(int N)
+    {
+        if( N == 0 ) return 0;
+        if( N == 1 ) return 1;
+        
+        int[] dp = new int[ N+1 ];
+        dp[ 0 ] = 0;
+        dp[ 1 ] = 1;
+        
+        for( int i = 2; i <= N; i++ )
+            dp[ i ] = dp[ i - 1 ] + dp[ i - 2 ];
+        
+        return dp[ N ];
+    }
+}
---
+++ Java/src/app/Q999_available_captures_for_rook.java
@@ -0,0 +1,37 @@
+package app;
+
+class Q999_available_captures_for_rook
+{
+    public int numRookCaptures(char[][] board)
+    {
+        if( board == null )     return 0;
+        if( board.length == 0 ) return 0;
+        
+        // find the rook
+        int x = 0, y = 0;
+        for( int i = 0; i < 8; i++ )
+            for( int j = 0; j < 8; j++ )
+            {
+                if( board[ i ][ j ] == 'R' )
+                {
+                    x = i;
+                    y = j;
+                    break;
+                }
+            }
+
+        // search for 4 directions
+        return search( board, x, y, -1, 0 ) + search( board, x, y, 1, 0 ) +
+               search( board, x, y, 0, -1 ) + search( board, x, y, 0, 1 );
+    }
+    
+    private int search( char[][] board, int i, int j, int x, int y )
+    {
+        if( (i + x) < 0 || (i + x) > 7 || (j + y) < 0 || (j + y) > 7 ) return 0;
+        // check what is found
+        if( board[ i + x ][ j + y ] == 'p' ) return 1;
+        if( board[ i + x ][ j + y ] == 'B' ) return 0;
+        
+        return search( board, i+x, j+y, x, y );
+    }
+}
