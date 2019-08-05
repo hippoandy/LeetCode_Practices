56ecd261f4bdd5fa95f22eda64c3a01ca0b20f18
hippoandy <ycaho@ucdavis.edu>
2019-08-05 16:17:21 -0700

    Revisited and add comment 20190805

--- Java/src/app/Q105_construct_binary_tree_from_preorder_and_inorder.java
+++ Java/src/app/Q105_construct_binary_tree_from_preorder_and_inorder.java
@@ -27,13 +27,16 @@ class Q105_construct_binary_tree_from_preorder_and_inorder
     private TreeNode helper( int parent, int left, int right, int[] preorder, int[] inorder )
     {
         if( parent >= preorder.length || left > right ) return null;
+        // preorder indicates the first element is the root of the tree
         TreeNode root = new TreeNode( preorder[ parent ] );
     
         int i = left;
         for( ; i < right; i++ )
         {
+            // find the root in the inorder array
             if( inorder[ i ] == root.val ) break;
         }
+        // partition the arry into left subtree and right subtree
         root.left = helper( parent + 1, left, i-1, preorder, inorder );
         root.right = helper( parent - left + i + 1, i+1, right, preorder, inorder );
         return root;
