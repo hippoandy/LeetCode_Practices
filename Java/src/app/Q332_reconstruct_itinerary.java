6b69deafb89d9ef2edc46ad44d1b25587fccf345
hippoandy <ycaho@ucdavis.edu>
2019-08-15 17:29:16 -0700

    Add new version of solution

--- Java/src/app/Q332_reconstruct_itinerary.java
+++ Java/src/app/Q332_reconstruct_itinerary.java
@@ -7,7 +7,8 @@ import java.util.*;
 
 class Q332_reconstruct_itinerary
 {
-    // use a queue to store the destinations
+    // use a priority queue to store the destinations
+    // requirement: should return the itinerary that has the smallest lexical order
     Map<String, PriorityQueue<String>> graph = new HashMap<String, PriorityQueue<String>>();
     List<String> route = new ArrayList<String>();
 
@@ -40,4 +41,40 @@ class Q332_reconstruct_itinerary
         // insert from the head since the recursion, the last airport will be at the head
         route.add( 0, airport );
     }
+
+    /* second version
+    // Runtime: 37 ms, faster than 14.54% of Java online submissions for Reconstruct Itinerary.
+    // Memory Usage: 40.7 MB, less than 79.10% of Java online submissions for Reconstruct Itinerary.
+    public List<String> findItinerary(List<List<String>> tickets)
+    {
+        if( tickets == null ) return null;
+        if( tickets.isEmpty() ) return null;
+        
+        // build the graph
+        Map<String, PriorityQueue<String>> graph = new HashMap<String, PriorityQueue<String>>();
+        for( List<String> t: tickets )
+        {
+            String src = t.get( 0 ), dst = t.get( 1 );
+            graph.computeIfAbsent( src, x -> new PriorityQueue<String>( new Comparator<String>()
+            {   @Override
+                public int compare( String a, String b )
+                // return the itinerary that has the smallest lexical order
+                { return a.compareTo( b ); }
+            }) ).offer( dst );
+        }
+
+        List<String> order = new ArrayList<String>();
+        // the itinerary must begin with "JFK"
+        dfs( graph, order, "JFK" );
+
+        return order;
+    }
+    
+    private void dfs( Map<String, PriorityQueue<String>> graph, List<String> order, String src )
+    {
+        while( graph.containsKey( src ) && !graph.get( src ).isEmpty() )
+            dfs( graph, order, graph.get( src ).remove() );
+        order.add( 0, src );
+    }
+    */
 }
