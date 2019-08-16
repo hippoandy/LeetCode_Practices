efae52bfcf9c59cf06c7916ca8490a38de66180f
hippoandy <ycaho@ucdavis.edu>
2019-08-15 17:08:54 -0700

    Upate logic

--- Java/src/app/Q56_merge_intervals.java
+++ Java/src/app/Q56_merge_intervals.java
@@ -11,27 +11,24 @@ class Q56_merge_intervals
         if( intervals == null ) return new int[ 0 ][ 0 ];
         if( intervals.length == 0 ) return new int[ 0 ][ 0 ];
 
-        Arrays.sort( intervals,
-            new Comparator<int[]>()
-            {   @Override
-                public int compare( int[] i1, int[] i2 )
-                {
-                    // sort by start
-                    return (i1[ 0 ] - i2[ 0 ]);
-                }
-            }
-        );
+        // sort the input by the start time
+        Arrays.sort( intervals, new Comparator<int[]>()
+        {   @Override
+            public int compare( int[] a, int[] b )
+            { return a[ 0 ] - b[ 0 ]; }
+        });
         
-        LinkedList<int[]> merged = new LinkedList<int[]>();
+        LinkedList<int[]> merge = new LinkedList<int[]>();
         for( int[] i: intervals )
         {
-            if( merged.size() == 0 || merged.getLast()[ 1 ] < i[ 0 ] )
-                merged.add( i );
+            // the very first element or the start of the next interval is larger than the current end
+            // no overlap!
+            if( merge.isEmpty() || merge.getLast()[ 1 ] < i[ 0 ] )
+                merge.add( i );
+            // there is an overlap
             else
-                // there is an overlap
-                merged.getLast()[ 1 ] = (i[ 1 ] > merged.getLast()[ 1 ]) ? i[ 1 ] : merged.getLast()[ 1 ];
+                merge.getLast()[ 1 ] = (merge.getLast()[ 1 ] > i[ 1 ]) ? merge.getLast()[ 1 ] : i[ 1 ];
         }
-        
-        return merged.toArray( new int[ merged.size() ][ 2 ] );
+        return merge.toArray( new int[ merge.size() ][ 2 ] );
     }
 }
