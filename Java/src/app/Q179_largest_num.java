cd0f2d45124414cdf82d4b787aceee25e069b3c3
hippoandy <ycaho@ucdavis.edu>
2019-08-16 18:53:19 -0700

    Revisited and new logic

--- Java/src/app/Q179_largest_num.java
+++ Java/src/app/Q179_largest_num.java
@@ -4,36 +4,57 @@ import java.util.*;
 
 class Q179_largest_num
 {
+    // version 2
     public String largestNumber(int[] nums)
     {
         String[] strs = new String[ nums.length ];
-        for( int i = 0; i < nums.length; i++ )
-            strs[ i ] = Integer.toString( nums[ i ] );
+        int idx = 0;
+        for( int i: nums ) strs[ idx++ ] = Integer.toString( i );
         
-        // sort the array with customized comparator
-        Arrays.sort( strs,
-            new largerFirstDigitString()
-        );
-        
-        // for case [ "0", "0" ]
+        // sort the array
+        Arrays.sort( strs, new Comparator<String>()
+        {   @Override
+            public int compare( String a, String b )
+            {   // larger combination first
+                String ab = a + b, ba = b + a;
+                return ba.compareTo( ab );
+            }
+        });
+        // consider case [ "0", "0" ]
         if( strs[ 0 ].equals( "0" ) ) return "0";
+        return String.join( "", strs );
+    }
+
+    // version 1
+    // public String largestNumber(int[] nums)
+    // {
+    //     String[] strs = new String[ nums.length ];
+    //     for( int i = 0; i < nums.length; i++ )
+    //         strs[ i ] = Integer.toString( nums[ i ] );
         
-        String ans = "";
-        for( String s: strs )
-            ans += s;
-        return ans;
-    }
-    
-    private class largerFirstDigitString implements Comparator<String>
-    {
-        @Override
-        public int compare( String a, String b )
-        {
-            String ab = a + b;
-            String ba = b + a;
-            return ba.compareTo( ab );
-        }
-    }
+    //     // sort the array with customized comparator
+    //     Arrays.sort( strs,
+    //         new largerFirstDigitString()
+    //     );
+        
+    //     // for case [ "0", "0" ]
+    //     if( strs[ 0 ].equals( "0" ) ) return "0";
+        
+    //     String ans = "";
+    //     for( String s: strs )
+    //         ans += s;
+    //     return ans;
+    // }
+    // private class largerFirstDigitString implements Comparator<String>
+    // {
+    //     @Override
+    //     public int compare( String a, String b )
+    //     {
+    //         String ab = a + b;
+    //         String ba = b + a;
+    //         return ba.compareTo( ab );
+    //     }
+    // }
 
 //     // Wrong Answer!!
 //     // the number couldn't be changed......
