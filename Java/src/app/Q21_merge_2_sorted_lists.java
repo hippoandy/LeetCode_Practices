814e568b7ebc0530d640d9c8f4a6631e5ca5f097
hippoandy <ycaho@ucdavis.edu>
2019-09-04 23:40:30 -0700

    New practice 20190904

---
+++ Java/src/app/Q21_merge_2_sorted_lists.java
@@ -0,0 +1,53 @@
+package app;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Q21_merge_2_sorted_lists
+{
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2)
+    {
+        ListNode dummy = new ListNode( -1 );
+        
+        ListNode ptr = dummy;
+        while( l1 != null && l2 != null )
+        {
+            if( l1.val <= l2.val )
+            {
+                ptr.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                ptr.next = l2;
+                l2 = l2.next;
+            }
+            ptr = ptr.next;
+        }
+        if( l1 != null ) appendList( ptr, l1 );
+        if( l2 != null ) appendList( ptr, l2 );
+        
+        return dummy.next;
+    }
+    
+    private void appendList( ListNode dest, ListNode src )
+    {
+        while( src != null )
+        {
+            dest.next = src;
+            dest = dest.next;
+            src = src.next;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
