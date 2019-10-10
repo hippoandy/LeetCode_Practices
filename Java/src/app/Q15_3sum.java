70225e026a73f3612c12f0c4117a68ba43df6dff
hippoandy <ycaho@ucdavis.edu>
2019-10-09 21:53:19 -0700

    Add comments

--- Java/src/app/Q15_3sum.java
+++ Java/src/app/Q15_3sum.java
@@ -4,36 +4,48 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-public class Q15_3sum {
+public class Q15_3sum
+{
     // two pointers
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+    public List<List<Integer>> threeSum(int[] nums)
+    {
+        List<List<Integer>> sol = new ArrayList<List<Integer>>();
+        if( nums == null || nums.length < 2 ) return sol;
+        
+        // sort the input array
         Arrays.sort( nums );
-
+        
+        // i + 2 is to make sure there are at least 3 elements
         for( int i = 0; i + 2 < nums.length; i++ )
         {
-            // If the number is the same as the number before, we have used it as target already, continue.
-            if( i > 0 && nums[ i ] == nums[ i - 1 ] ) continue;
-            // set the pointers
-            int j = i + 1, k = (nums.length - 1);
+            if( i > 0 && nums[ i ] == nums[ i-1 ] ) continue;
+
+            // since the target is zero
+            // choose the current i as one of the selected element
             int target = -nums[ i ];
-            // (j < k): condition for termination
-            while( j < k )
+
+            // find the solution between i+1 ~ n-1
+            int l = i+1, r = nums.length-1;
+            while( l < r )
             {
-                if( nums[ j ] + nums[ k ] == target )
+                // found a solution
+                if( target == nums[ l ] + nums[ r ] )
                 {
-                    result.add( Arrays.asList( nums[ i ], nums[ j ], nums[ k ] ) );
-                    j++;
-                    k--;
-                    // prevent using same number as target
-                    // (j < k): condition for termination
-                    while( j < k && nums[ j ] == nums[ j - 1 ] ) j++;
-                    while( j < k && nums[ k ] == nums[ k + 1 ] ) k--;
+                    sol.add( Arrays.asList( nums[ i ], nums[ l ], nums[ r ] ) );
+                    l++;
+                    r--;
+                    
+                    // prevent using the same number as target
+                    // that's why the array should be sorted beforehand
+                    while( l < r && nums[ l ] == nums[ l - 1 ] ) l++;
+                    while( l < r && nums[ r ] == nums[ r + 1 ] ) r--;
                 }
-                else if( nums[ j ] + nums[ k ] > target ) k--;
-                else j++;
+                // the sum is too big, substract the right pointer
+                else if( nums[ l ] + nums[ r ] > target ) r--;
+                // the sum is too small, increase the left pointer
+                else l++;
             }
         }
-        return result;
+        return sol;
     }
 }
